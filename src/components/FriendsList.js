198112bd1c90412995d8c840f050eea393426ff8
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-08 12:41:16 -0600

    wip

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -11,7 +11,7 @@ export default function FriendsList(props) {
       {/* 👉 3- We need to loop over the data rendering a Friend as we go */}
       {/* Each friend is going to need a `key` prop and also some other prop with data */}
       {friends.map((friendObj) => {
-        return <div>{friendObj.married}</div>;
+        return <Friend />;
       })}
     </div>
   );
